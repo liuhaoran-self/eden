5f065e8756c7cec2e291f003ca32a1530b2eca4a
Main  PC <liuhaoran2007@gmail.com>
2021-07-04 02:03:23 +0800

    Add weapon effect

--- src/main/java/xyz/cbfh/eden/EdenItem.java
+++ src/main/java/xyz/cbfh/eden/EdenItem.java
@@ -1,8 +1,12 @@
 package xyz.cbfh.eden;
 
+import java.lang.Override;
 import net.minecraft.item.*;
+import net.minecraft.item.ItemStack;
 import net.minecraft.sound.*;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
@@ -12,10 +16,11 @@ public class EdenItem extends Item{
 	public EdenItem(Settings settings) {
 		super(settings);
 	}
-	@Override
-	public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
-		playerEntity.playSound(SoundEvents.ENTITY_WITHER_SPAWN, 1.0F, 1.0F);
-		return TypedActionResult.success(playerEntity.getStackInHand(hand));
+
+	public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
+		entity.playSound(SoundEvents.ENTITY_CHICKEN_AMBIENT, 1.0F, 1.0F);
+		entity.age++;
+		return ActionResult.PASS;
 	}
 	
 }
--- src/main/java/xyz/cbfh/eden/Tools/Sword.java
+++ src/main/java/xyz/cbfh/eden/Tools/Sword.java
@@ -2,6 +2,8 @@ package xyz.cbfh.eden.Tools;
 
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
@@ -13,7 +15,7 @@ import xyz.cbfh.eden.Main;
 public class Sword extends SwordItem {
 
     public Sword(ToolMaterial material) {
-        super(material, 0, -3.9f, new Item.Settings().group(Main.EDEN_THINGS).rarity(Rarity.EPIC));//伤害值相较ToolFabric中的4-1=3，攻击速度-2.2，设置在fabric_example_group中
+        super(material, +9, +6.0f, new Item.Settings().group(Main.EDEN_THINGS).rarity(Rarity.EPIC));//伤害值相较ToolFabric中的4-1=3，攻击速度-2.2，设置在fabric_example_group中
     }
     
     @Override
@@ -21,7 +23,13 @@ public class Sword extends SwordItem {
         stack.damage(1, (LivingEntity)attacker, ((e) -> {
            e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND);
         }));
-        if(target.isDead())target.playSound(SoundEvents.ENTITY_WITHER_DEATH, 1.0F, 1.0F);
+        if(target.isAlive()){ 
+            attacker.addStatusEffect(new StatusEffectInstance(StatusEffects.INVISIBILITY,100,1,true,true,true));
+            target.playSound(SoundEvents.BLOCK_ANVIL_LAND, 1.0F, 1.0F);
+            target.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS,100,255));
+            target.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS,100,1));
+            target.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING,100,1));
+        }
         return true;
      }
     // public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
